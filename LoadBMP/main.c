049974bc5c4b9bd5afe838904e02faa0fbe12a94
yunp <xtiqin@163.com>
2020-11-30 22:15:11 -0800

    添加关于渲染器使用的例子

--- LoadBMP/main.c
+++ LoadBMP/main.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
 
 #define WIDTH 600
 #define HEIGHT 500
 #define FRAMERATE 60
-#define FONT_SIZE 32
 
-TTF_Font *font;
 
+struct SDL_Surface *img;
 
 void draw(SDL_Surface *screen, SDL_Window *win) {
-    SDL_Color color = {255, 255, 255, 255};
-    struct SDL_Surface *text = TTF_RenderUTF8_Blended(
-            font, "https://yunp.top", color
-    );
-    SDL_Rect src = {0, 0, text->w, text->h};
-    SDL_BlitSurface(text, &src, screen, &src);
-    SDL_FreeSurface(text);
+
+    SDL_Rect src = {0, 0, img->w, img->h};
+    SDL_BlitSurface(img, &src, screen, &src);
+
     SDL_UpdateWindowSurface(win);
 }
 
@@ -44,11 +39,6 @@ void event_loop(SDL_Surface *screen, SDL_Window *win) {
 }
 
 int main() {
-    if (TTF_Init()) {
-        SDL_Log("Can not init ttf, %s", TTF_GetError());
-        return 1;
-    }
-
     if (SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Can not init video, %s", SDL_GetError());
         return 1;
@@ -66,15 +56,11 @@ int main() {
         return 1;
     }
 
-    font = TTF_OpenFont("padmaa.ttf", FONT_SIZE);
-    if (font == NULL) {
-        SDL_Log("Can not open font");
-        return 1;
-    }
+    img = SDL_LoadBMP("cat.bmp");
     struct SDL_Surface *screen = SDL_GetWindowSurface(win);
 
     event_loop(screen, win);
-    TTF_CloseFont(font);
+    SDL_FreeSurface(img);
     SDL_DestroyWindow(win);
     return 0;
 }
---
+++ RenderImage/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define WIDTH 400
+#define HEIGHT 300
+#define FRAMERATE 60
+#define FILE_NAME "cat.bmp"
+
+SDL_Renderer *renderer;
+SDL_Window *window;
+SDL_Texture *imageTexture;
+
+void draw() {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+//    int w, h;
+//    SDL_QueryTexture(imageTexture, NULL, NULL, &w, &h);
+    SDL_Rect destRect = {0, 0, 100, 100};
+    SDL_RenderCopy(renderer, imageTexture, NULL, &destRect);
+
+    SDL_RenderPresent(renderer);
+}
+
+void event_loop() {
+    while (1) {
+        uint32_t begin = SDL_GetTicks();
+        draw();
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                return;
+            }
+        }
+        uint32_t current = SDL_GetTicks();
+        uint32_t cost = current - begin;
+        uint32_t frame = 1000 / FRAMERATE;
+        uint32_t delay = frame - cost;
+
+        if (delay > 0) {
+            SDL_Delay(delay);
+        }
+    }
+}
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Can not init video, %s", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow(
+            "Hello World",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            WIDTH, HEIGHT,
+            SDL_WINDOW_SHOWN
+    );
+    if (window == NULL) {
+        SDL_Log("Can not create window, %s", SDL_GetError());
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        SDL_Log("Can not create renderer, %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Surface *imageSurface = SDL_LoadBMP(FILE_NAME);
+    if (imageSurface == NULL) {
+        SDL_Log("Can not load image, %s", SDL_GetError());
+        return 1;
+    }
+    imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+    SDL_FreeSurface(imageSurface);
+
+    event_loop();
+
+    SDL_DestroyTexture(imageTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    return 0;
+}
